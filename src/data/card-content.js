35c95621282e84cfd5d71cbd0e6090ecf1b340ad
Sebastian Elias <seen0001@stud.kea.dk>
2024-12-19 14:27:07 +0100

    first commit

---
+++ src/data/card-content.js
@@ -0,0 +1,22 @@
+export const cardContent = [
+  {
+    image: "/src/icons/balance-sheet.svg",
+    title: "The Balance Sheet",
+    body: "The Balance Sheet is a summary of the assets and liabilities and equity of a business at a specific point of time.",
+  },
+  {
+    image: "/src/icons/income.svg",
+    title: "Income Statement",
+    body: "The Income (Profit and Loss) Statement, commonly referred to as the P&L statement, summarizes the revenue and expenses for a specific time period (one month, one quarter, one year, etc.)",
+  },
+  {
+    image: "/src/icons/financial.svg",
+    title: "Financial Plan",
+    body: "Keep your information current and review the documents on a regular basis (monthly or more often if needed). Review them with key individuals within your company.",
+  },
+  {
+    image: "/src/icons/ratios.svg",
+    title: "Financial Ratios",
+    body: "Ratios are useful when comparing your company with the competition on financial performance and also when benchmarking the performance of your company.",
+  },
+];
